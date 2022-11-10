4f3644e92672c06d073889cd04e9c983ff9e3dfe
Gyeonghun Lee <kang000322@gmail.com>
2022-11-10 15:17:02 +0900

    Changed structure

---
+++ Java Project/src/gui/mainFrame.java
@@ -0,0 +1,29 @@
+package gui;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class mainFrame extends JFrame {
+	public mainFrame()
+	{
+		
+	}
+	class FirstGui extends JPanel{
+		public FirstGui()	
+		{
+			
+		}
+	}
+	class SecondGui extends JPanel{
+		public SecondGui()	
+		{
+			
+		}
+	}
+	class ThridGui extends JPanel{
+		public ThridGui()
+		{
+			
+		}
+	}
+}
